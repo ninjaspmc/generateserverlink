ab0709c9eac63501b6ca5639ddf97e2f1d790cf5
Ninja Spmc <ninjaspmc@gmail.com>
2024-12-04 15:18:27 -0300

    first commit

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/',(req, res) => {
+res.sendFile(path.join(__dirname, "./public/", "index.html"))});
 
 app.get('/add', (req, res) => {
     const { nome, ip, porta } = req.query;
     if (!nome || !ip || !porta || isNaN(porta) || parseInt(porta) < 1 || parseInt(porta) > 65535 || /\s/.test(ip)) {
-        return res.status(400).send('Parâmetros inválidos. Verifique o nome, IP e porta.');
+            return res.status(400).send('Parâmetros inválidos. Verifique o nome, IP e porta.');
     }
     const minecraftLink = `minecraft://?addExternalServer=${encodeURIComponent(nome)}|${encodeURIComponent(ip)}:${encodeURIComponent(porta)}`;
     res.redirect(minecraftLink);
